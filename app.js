42e85464241cb52d771238c9c909cd9aec015186
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-07-28 20:11:24 -0400

    fixed post for login

--- app.js
+++ app.js
@@ -11,6 +11,31 @@ app.use(bodyParser.json())
 
 const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
-    db.run(“CREATE TABLE user(username TEXT, password TEXT, title TEXT)“);
-    db.run(“INSERT INTO user VALUES('privilegedUser', 'privilegedUser1', 'Administrator')“);
+    db.run('CREATE TABLE user(username TEXT, password TEXT, title TEXT)');
+    db.run("INSERT INTO user VALUES('privilegedUser', 'privilegedUser1', 'Administrator')");
 });
+
+app.get('/', function(req,res) {
+    res.sendFile('index.html')
+})
+
+app.post('/login', function (req,res) {
+    var username = req.body.username; 
+    var password = req.body.passward;
+
+    var query = "SELECT title FROM user WHERE username = " + username + " and password = " + password ; 
+
+    db.get(query, function (err, row) {
+
+        if (err) {
+            console.log('ERROR', err);
+            res.redirect('/index.html#error');
+        } else if (!row) {
+            res.redirect('/index.html#unauthorized');
+        } else {
+            res.send('Hello <b>' + row.title + '!</b><br/>This file contains all your secret data: <br/><br/> SECRETS <br /><br /> MORE SECRETS <br/><br/><a href=“/index.html”>Go back to login</a>')
+        }
+    });
+});
+
+app.listen(3000, () => {console.log('Listen!')})
